20befa6777bb7bde92aaf972a58a027a93cdb47c
Xizhou Li <xl006@bucknell.edu>
2018-07-27 17:45:26 -0700

    basic video and image rendering complete

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,6 +5,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
+      thumbnails: [1,2,3,4,5,6,7,8,9,10],
       repos: []
     } 
   }
@@ -16,11 +17,18 @@ class App extends React.Component {
             <div className='row'>
               <div className='col left'>
 
-                <div id='video'>
+                <div>
+                  <embed id='video' src="https://steamcdn-a.akamaihd.net/steam/apps/256720476/movie480.webm?t=1529548698" />
                 </div>
 
                 <div id='list'>
-                  <div id='scroll'>              
+                  <div id='scroll'> 
+                    {
+                      this.state.thumbnails.map( (i) => {
+                        var url = "https://s3-us-west-1.amazonaws.com/steamgallery/pubg"+i+".jpg";
+                        return <img className="thumbnails" key={i} src={url} />
+                      })
+                    }             
                   </div>
                 </div>
 
